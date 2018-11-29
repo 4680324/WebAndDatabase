a16e388795baa00dd20c289a1d0926769d80953e
Hytak <lhc.holten@gmail.com>
2018-11-29 17:16:21 +0100

    coords

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,7 +2,13 @@ var main = function(){   //this is the main
     "use strict";
     
         $("div.row>div").on("click", function (event) {
-            console.log("div click");
+            console.log(event.target);
+            var node = event.target;
+            for (var column=0; (node=node.previousSibling); column++);
+            var parent = event.target.parentNode;
+            for (var row=0; (parent=parent.previousSibling); row++);
+            row = (row + 1) / 2;
+            console.log("column: %s, row %s: div click", column, row);
         });
     
         $("#resignButton").on("click", function(event) {
