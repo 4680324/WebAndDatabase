cd737e9536a822f25b71d67162c0e2437d844022
Raoul Kalkman <raoulkalkman@outlook.com>
2018-11-29 16:52:12 +0100

    lil bug fix regarding javascript/JQuery linking

--- public/javascripts/splashJS.js
+++ public/javascripts/splashJS.js
@@ -1,11 +1,11 @@
 var main = function(){   //this is the main
 "use strict";
 
-    $(".Stranger button").on("click", function (event) {
+    $("#Stranger button").on("click", function (event) {
         console.log("dit werkt 1");
     });
 
-    $(".Friend button").on("click", function(event) {
+    $("#Friend button").on("click", function(event) {
         console.log("Dit werkt niet lol");
     });
 
