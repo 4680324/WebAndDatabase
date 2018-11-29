26d6cf3e7601a359d09925802e8ac785d1e773a3
Raoul Kalkman <raoulkalkman@outlook.com>
2018-11-29 16:34:16 +0100

    Linked the splashJS to splash html + added JQuery lib to splash

---
+++ public/splashJS.js
@@ -0,0 +1,13 @@
+var main = function(){   //this is the main
+"use strict";
+
+    $(".Stranger button").on("click", function (event) {
+        console.log("dit werkt 1");
+    });
+
+    $(".Friend button").on("click", function(event) {
+        console.log("Dit werkt niet lol");
+    });
+};
+
+$(document).ready(main);
