5eb8e32b77f6ebf9330f6135a91cb7b50f57548a
Hytak <lhc.holten@gmail.com>
2018-12-06 15:08:30 +0100

    reindent and use ws in the client

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,23 +1,30 @@
 var main = function(){   //this is the main
     "use strict";
     
-        $("div.row>div").on("click", function (event) {
-            console.log(event.target);
-            var node = event.target;
-            for (var column=0; (node=node.previousSibling); column++);
-            var parent = event.target.parentNode;
-            for (var row=0; (parent=parent.previousSibling); row++);
-            row = (row + 1) / 2;
-            console.log("column: %s, row %s: div click", column, row);
-        });
-    
-        $("#resignButton").on("click", function(event) {
-            console.log("you resigned!");
-        });
-    
-        $("#offerDrawButton").on("click", function(event) {
-            console.log("you offered a draw");
-        });
+    $("div.row>div").on("click", function (event) {
+        console.log(event.target);
+        var node = event.target;
+        for (var column=0; (node=node.previousSibling); column++);
+        var parent = event.target.parentNode;
+        for (var row=0; (parent=parent.previousSibling); row++);
+        row = (row + 1) / 2;
+        console.log("column: %s, row %s: div click", column, row);
+    });
+
+    $("#resignButton").on("click", function(event) {
+        console.log("you resigned!");
+    });
+
+    $("#offerDrawButton").on("click", function(event) {
+        console.log("you offered a draw");
+    });
+
+    var socket = new WebSocket("ws://localhost:3000");
+
+    socket.onmessage = function(event){}
+    socket.onopen = function(){
+        socket.send("Hello from the client!");
     };
+};
 
 $(document).ready(main);
