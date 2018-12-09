6e0a504d864703c9c2479fdc4a2702ae9c09a236
Raoul Kalkman <raoulkalkman@outlook.com>
2018-12-09 23:30:06 +0100

    Added the render of the board and move msg.

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,27 +1,32 @@
 //global variable socket
 var socket = new WebSocket("ws://localhost");
 var color = null;
-
+var board = new logic.BoardState(); //creates begin state
 
 var main = function(){ 
     "use strict";
 
+
     socket.onopen = function(){ //when opening a connection, send a message with the name of the connecting party to the server.
         // socket.send("Hello from the client!");
-
+        
         var user = prompt("Please enter your name");
 
         if(user !== ""){
             var startGame = new messages.O_GAME_START(user, null);
             socket.send(JSON.stringify(startGame));
             //show own name in the name tag in html
-        
+            
+
             $("#Name1").text(user);
 
         }else{
-            console.log("disconnect!");
-         //   returntosplash();
+            var startGame = new messages.O_GAME_START('verlegen man', null);
+            socket.send(JSON.stringify(startGame));
+
+            $('#Name1').text('verlegen man');
         }
+        update_interface(); //makes sure the classes get initialized.
     };
 
     socket.onmessage = function(event){
@@ -35,22 +40,38 @@ var main = function(){
         if(mess.type === messages.T_MOVE){
             move = mess.move;
             update = mess.update;
+
+            var notColor = null;
+
+            if(color === "white"){
+                notColor = "black";
+            }else{
+                notColor = "white";
+            }
+            
+            board.move(move.x, move.y, notColor);
+            update_interface();
         }
 
         socket.send();
     };
 
-    var returnToSplash = function(){
-
+    var update_interface = function(){
+        Object.values(this.state).forEach(line => {
+            Object.values(line).forEach(pos => {
+                if (pos === 'black') {
+                    $(/*==>desbetreffende tb */).removeClass();  //have to change selector!!
+                    pos.addClass("active-Black");
+                } else if (pos === 'white') {       
+                    $(".tabs span").removeClass();  //have to change selector!!
+                    pos.addClass("active-White");
+                }
+            });
+        });
+        return {black: black, white: white};
     };
 
-
-<<<<<<< HEAD
-
-    $("div.row>div").on("click", function (event) {
-=======
     $("td").on("click", function (event) {
->>>>>>> a7e47be4d1baaba92a64f1c263221e38d33c37b7
         var node = event.target;
         for (var column=0; (node=node.previousSibling); column++);
         column = (column + 1) / 2;
@@ -58,13 +79,19 @@ var main = function(){
         for (var row=0; (parent=parent.previousSibling); row++);
         row = row / 2 + 1;
         console.log("column %s, row %s", column, row);
+        if(board.checkMove){
+            var move = new messages.O_MOVE({x:column, y:row}, null);
+            socket.send(JSON.stringify(move));
+            board.move(column, row, color);
+            update_interface();
+        }
     });
 
     //send a JSON file to the server which gives a resign message
     $("#resignButton").on("click", function(event) {
         console.log("you resigned!");
         // socket.send("played disconnected");
-        socket.close();
+        socet.close();
 
         $(".chat").attr("value", "You resigned, feelsbadman")
     });
